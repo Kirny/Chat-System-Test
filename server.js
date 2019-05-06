f131e157f9c64be11c7aff28118270b03ef289bd
Felix Traing <kirnyx3@gmail.com>
2019-05-05 23:41:55 -0400

    long due commit, added npm initialize for concurrent server + client

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
-server.listen(process.env.PORT || 3000);
+server.listen(process.env.PORT || 3001);
 
 app.get('/', function (req, res) {
   //res.sendFile(__dirname + '/index.html');
@@ -12,6 +12,8 @@ app.get('/', function (req, res) {
 io.on('connection', function (socket) {
   socket.emit('news', { hello: 'world' });
   socket.on('my other event', function (data) {
-    console.log(data);
+  	console.log(data);
   });
+  console.log('a connection has been established');
+    
 });
